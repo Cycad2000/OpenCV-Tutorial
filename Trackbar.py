8fd0aa39067f8833aa94f6e0f517e38eb4f95335
Cycad2000 <68537526+Cycad2000@users.noreply.github.com>
2020-07-20 19:01:17 +0900

    complete Trackbar.py

--- Trackbar.py
+++ Trackbar.py
@@ -1,24 +1,11 @@
 import cv2
 import numpy as np
 
-def nothing(x):
-    pass
-
-
-# Create a black image, a window
-img = np.zeros((300,512,3), np.uint8)
-cv2.imshow('image',img)
 drawing = False # true if mouse is pressed
 mode = True # if True, draw rectangle. Press 'm' to toggle to curve
-
-# create trackbars for color change
-cv2.createTrackbar('R','image',0,255,nothing)
-cv2.createTrackbar('G','image',0,255,nothing)
-cv2.createTrackbar('B','image',0,255,nothing)
-
-# create switch for ON/OFF functionality
-switch = '0 : OFF \n1 : ON'
-cv2.createTrackbar(switch, 'image',0,1,nothing)
+ix,iy = -1,-1
+def nothing(x):
+    pass
 
 def draw_circle(event,x,y,flags,param):
     global ix,iy,drawing,mode
@@ -32,7 +19,7 @@ def draw_circle(event,x,y,flags,param):
             if mode == True:
                 cv2.rectangle(img,(ix,iy),(x,y),(b,g,r),-1)
             else:
-                cv2.circle(img,(x,y),5,(b,g,r),-1)
+                cv2.circle(img,(x,y),rad,(b,g,r),-1)
 
     elif event == cv2.EVENT_LBUTTONUP:
         drawing = False
@@ -40,8 +27,18 @@ def draw_circle(event,x,y,flags,param):
             cv2.rectangle(img,(ix,iy),(x,y),(b,g,r),-1)
         else:
             cv2.circle(img,(x,y),5,(b,g,r),-1)
+# Create a black image, a window
+img = np.zeros((300,512,3), np.uint8)
+cv2.namedWindow('image')
 cv2.setMouseCallback('image',draw_circle)
+# create trackbars for color change
+cv2.createTrackbar('R','image',0,255,nothing)
+cv2.createTrackbar('G','image',0,255,nothing)
+cv2.createTrackbar('B','image',0,255,nothing)
+cv2.createTrackbar('Radius', 'image',1,50,nothing)
+
 while(1):
+    cv2.imshow('image',img)
     k = cv2.waitKey(1) & 0xFF
     if k == 27:
         break
@@ -50,11 +47,11 @@ while(1):
     r = cv2.getTrackbarPos('R','image')
     g = cv2.getTrackbarPos('G','image')
     b = cv2.getTrackbarPos('B','image')
-    s = cv2.getTrackbarPos(switch,'image')
+    rad = cv2.getTrackbarPos('Radius','image')
 
-    if s == 0:
-        img[:] = 0
-    else:
-        img[:] = [b,g,r]
-
+    k = cv2.waitKey(1) & 0xFF
+    if k == ord('m'):
+        mode = not mode
+    elif k == 27:
+        break
 cv2.destroyAllWindows()
